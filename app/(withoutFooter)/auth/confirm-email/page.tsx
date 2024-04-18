2abbb4e8f635492e7b6d40e580fe5603cf4ebc01
juanm512 <512juanm@gmail.com>
2024-04-18 12:09:27 -0300

    fix: Confirm email enlaces dirigidos correctamente a login e inicio

--- app/(withoutFooter)/auth/confirm-email/page.tsx
+++ app/(withoutFooter)/auth/confirm-email/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Page() {
   return (
     <main className="flex items-center justify-center h-[80dvh] place-items-center px-6 py-24 sm:py-32 lg:px-8">
@@ -11,15 +13,15 @@ export default function Page() {
           electronico.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
+          <Link
+            href="/auth/login"
             className="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
           >
             Iniciar sesion
-          </a>
-          <a href="#" className="text-sm font-semibold text-gray-900">
+          </Link>
+          <Link href="#" className="text-sm font-semibold text-gray-900">
             Ir al inicio <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </div>
     </main>
