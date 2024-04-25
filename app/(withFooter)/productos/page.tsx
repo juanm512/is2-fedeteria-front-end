ab0174fdf34a6c789738a67addc0516d866c5d6c
juanm512 <512juanm@gmail.com>
2024-04-25 01:04:16 -0300

    fix: deployment fix y cambiando estilos de los productos

--- app/(withFooter)/productos/page.tsx
+++ app/(withFooter)/productos/page.tsx
@@ -61,29 +61,43 @@ async function ProductsList({
 
   return (
     <div className="lg:col-span-3">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-2 py-16 sm:px-3 sm:py-24 lg:max-w-7xl lg:px-4">
         <h2 className="sr-only">Products</h2>
 
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-x-4 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-6">
           {/*  */}
-          {productos.map((product) => (
+          {productos.map((product: any) => (
             <Link
               key={product.id}
               href={'/productos/' + product.id}
               className="group/product"
             >
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+              <div className="relative aspect-h-3 aspect-w-2 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-9 xl:aspect-w-7">
                 <Image
                   src={product.imageUrl}
                   fill
                   alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-                  className="h-full w-full object-cover object-center group-hover/product:opacity-75"
+                  className="h-full w-full rounded-xl object-cover object-center group-hover/product:opacity-75"
                 />
+                <div className="absolute inset-0 flex flex-col-reverse">
+                  <div className="relative w-full h-11 p-2 bg-accent-300/90 backdrop-blur-md rounded-t-xl transition-all duration-300 delay-75 ease-in group-hover/product:h-24 pointer-events-none">
+                    <div className="flex justify-between items-start">
+                      <h3 className="text-base font-medium text-gray-700 leading-7">
+                        {product.title}
+                      </h3>
+                      <p className="flex-shrink-0 text-xl font-medium text-gray-900">
+                        ${product.price}
+                      </p>
+                    </div>
+                    <div className="opacity-0 translate-y-full flex justify-start items-center gap-4 transition-all will-change-transform duration-300 delay-150 ease-out group-hover/product:translate-y-0 group-hover/product:opacity-100 ">
+                      <p className="flex-shrink-0 text-sm text-gray-900">
+                        Vendido por
+                      </p>
+                      <h3 className="text-base text-gray-900">Felipee</h3>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                ${product.price}
-              </p>
             </Link>
           ))}
 
